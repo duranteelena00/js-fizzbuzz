25fa2f3e4a37c2661b84da339807969a75ca502b
durante.elena00 <durante.elena00@gmail.com>
2021-07-06 20:21:43 +0200

    aggiunzione (stampa della lista in pagina)

--- js/script.js
+++ js/script.js
@@ -2,15 +2,24 @@
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
+var display = document.getElementById("demo");
 var num = 0;
-for (i = 1; i <= 100; i++) {
+
+listElement = "";
+for (var i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("fizzBuzz");
+    console.log("FizzBuzz");
+    listElement += "<li>FizzBuzz</li>";
   } else if (i % 3 === 0) {
     console.log("Fizz");
+    listElement += "<li>Fizz</li>";
   } else if (i % 5 === 0) {
     console.log("Buzz");
+    listElement += "<li>Buzz</li>";
   } else {
     console.log(i);
+    listElement += "<li>" + i + "</li>";
   }
 }
+
+display.innerHTML = listElement
